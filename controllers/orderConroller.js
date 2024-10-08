8810561e1d0ef5a373c83711cb0ff6bb1f303c39
muhammad haseen <hasinmmpm786@gmail.com>
2024-10-08 15:51:50 +0530

    updated order get

--- controllers/orderConroller.js
+++ controllers/orderConroller.js
@@ -1,75 +1,5 @@
-// // import OrderItem from "../Models/orderItemsModel.js";
 import Order from "../Models/orderModel.js";
-// import jwt from "jsonwebtoken";
-// import "dotenv/config";
-// import razorpayInstance from "../config/razorPay.js";
-// import {User} from "../Models/userModel.js"
 
-
-
-// // get all orders
-
-// const getAllOrders = async (req, res) => {
-//   try {
-//     const orders = await Order.find().populate("user", "name");
-
-//     if (orders.length == 0) {
-//       return res.status(404).json({ message: "No orders found" });
-//     }
-//     res.json(orders);
-//   } catch (error) {
-//     res
-//       .status(500)
-//       .json({ message: "An error occurred", error: error.message });
-//   }
-// };
-
-// // update order
-
-// const updateOrder = async (req, res) => {
-//   try {
-//     const Id = req.params.id;
-//     const status = req.body;
-//     console.log(Id);
-//     const updatedOrder = await Order.findByIdAndUpdate(Id, status, {
-//       new: true,
-//     });
-
-//     if (!updatedOrder) {
-//       return res.status(404).json({ message: "Order not found" });
-//     }
-
-//     res.json(updatedOrder);
-//   } catch (error) {
-//     console.log(error);
-//     res
-//       .status(500)
-//       .json({ message: "An error occurred", error: error.message });
-//   }
-// };
-
-// // delete order
-
-// const deleteOrder = async (req, res) => {
-//   try {
-//     const id = req.params.id;
-//     const deletedOrder = await Order.findByIdAndDelete(id);
-
-//     if (!deletedOrder) {
-//       return res.status(404).json({ message: "Order not found" });
-//     }
-
-//     res.json({ message: "Order deleted successfully" });
-//   } catch (error) {
-//     res
-//       .status(500)
-//       .json({ message: "An error occurred", error: error.message });
-//   }
-// };
-
-
-
-// // delete all 
 
 const deleteAll =async (req,res)=>{
   try {
@@ -81,6 +11,7 @@ const deleteAll =async (req,res)=>{
      .json({ message: "An error occurred", error: error.message });
   }
 }
+
 
 
 
--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -105,31 +105,6 @@ const verifyPayment = async (req, res) => {
   }
 };
 
-// Route to update order after payment confirmation
-// const saveOrder = async (req, res) => {
-//   const { paymentStatus } = req.body;
-//   const { id } = req.params; // Order ID
-
-//   try {
-//     const updatedOrder = await Order.findByIdAndUpdate(
-//       id,
-//       { payment: paymentStatus },
-//       { new: true }
-//     );
-
-//     if (!updatedOrder) {
-//       return res.status(404).json({ error: "Order not found" });
-//     }
-
-//     res
-//       .status(200)
-//       .json({ message: "Order saved successfully", order: updatedOrder });
-//   } catch (error) {
-//     console.error("Error saving order:", error);
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
 const deleteOrder = async (req, res) => {
   const { id } = req.params; // Order ID
   try {
@@ -144,23 +119,22 @@ const deleteOrder = async (req, res) => {
   }
 };
 // All order
-const getAllOrders = async (req, res) => {
+const getOrders = async (req, res) => {
   try {
-    const orders = await Order.find({});
+    const orders = await Order.find({}).sort({ dateCreated: -1 });
     res.status(200).json(orders);
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
 
 // Update status
 const updateStatus = async (req, res) => {
-  const { id, status } = req.body; 
+  const { id, status } = req.body;
 
   try {
-    console.log("id is",id)
-    const findOrder = await Order.findById(id); 
+    console.log("id is", id);
+    const findOrder = await Order.findById(id);
     if (!findOrder) {
       return res.status(404).json({ error: "Order not found" });
     }
@@ -168,8 +142,8 @@ const updateStatus = async (req, res) => {
     // Update the status of the order
     const updatedOrder = await Order.findByIdAndUpdate(
       id,
-      { status: status }, 
-      { new: true } 
+      { status: status },
+      { new: true }
     );
 
     if (!updatedOrder) {
@@ -184,106 +158,4 @@ const updateStatus = async (req, res) => {
   }
 };
 
-
-export { orderSetup, verifyPayment, deleteOrder, getAllOrders,updateStatus };
-
-// const orderSetup = async (req, res) => {
-//   const id = req.params.id;
-//   try {
-//     // Create a new order with userId, items, amount, and address
-//     const newOrder = new Order({
-//       userId: id,
-//       items: req.body.orderItems,
-//       amount: req.body.totalAmount,
-//       address: req.body.address,
-//       city: req.body.city,
-//       district: req.body.district,
-//     });
-
-//     // Save the new order to the database
-//     const savedOrder = await newOrder.save();
-//     // console.log("savedOrder",savedOrder);
-
-//     // const deliveryFee = req.body.totalAmount > 500 ? 20 * 100 : 50 * 100;
-//     const totalAmount = req.body.totalAmount * 100; //+ deliveryFee; // Amount in paise
-//     const options = {
-//       amount: totalAmount,
-//       currency: "INR",
-//       receipt: crypto.randomBytes(10).toString("hex"), // Generate random receipt ID
-//     };
-
-//     // Create the Razorpay order
-//     razorpayInstance.orders.create(options, (error, order) => {
-//       if (error) {
-//         console.log(error);
-//         return res.status(400).json({ error: error.message });
-//       }
-//       // console.log("order", order);
-//       return res.status(200).json({ order, id: savedOrder._id });
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(500).json({ error: error.message });
-//   }
-// };
-
-// const verifyPayment = async (req, res) => {
-//   const id = req.params.id;
-//   try {
-//     // Extract razorpay data from the request body
-//     const {
-//       razorpay_order_id,
-//       razorpay_payment_id,
-//       razorpay_signature,
-//       order_id,
-//     } = req.body;
-//     console.log(req.body);
-
-//     // Check if all required fields are present
-//     if (!razorpay_order_id || !razorpay_payment_id || !razorpay_signature) {
-//       return res.status(400).json({ error: "Missing payment details" });
-//     }
-
-//     const secret = process.env.Key_SECRET;
-//     console.log("secret", secret);
-
-//     // Generate HMAC with the received order_id and payment_id
-//     // const hmac = crypto.createHmac("sha256", secret);
-//     // hmac.update(razorpay_order_id + "|" + razorpay_payment_id);
-//     // const generated_signature = hmac.digest("hex");
-//     const generated_signature = crypto
-//       .createHmac("SHA256", secret)
-//       .update(razorpay_order_id + "|" + razorpay_payment_id)
-//       .digest("hex");
-
-//     console.log("Razorpay signature from frontend:", razorpay_signature);
-//     console.log("Generated signature on backend:", generated_signature);
-
-//     // Compare generated signature with the received signature
-//     if (generated_signature === razorpay_signature) {
-//       console.log("Payment verification successful");
-//       await User.findByIdAndUpdate(id, { cartData: {} });
-//       // Update the order's payment status
-//       const updatedOrder = await Order.findByIdAndUpdate(
-//         order_id,
-//         { payment: true },
-//         { new: true }
-//       );
-
-//       console.log(order_id);
-//       if (!updatedOrder) {
-//         return res.status(404).json({ error: "Order not found" });
-//       }
-
-//       return res.status(200).json({ message: "Payment successful" });
-//     } else {
-//       console.log("Payment verification failed. Signatures don't match.");
-//       return res.status(403).json({
-//         message: "Payment verification failed. Signatures don't match.",
-//       });
-//     }
-//   } catch (error) {
-//     console.error("Error during payment verification:", error);
-//     return res.status(500).json({ error: error.message });
-//   }
-// }
+export { orderSetup, verifyPayment, deleteOrder, getAllOrders, updateStatus };
